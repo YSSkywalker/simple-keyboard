ff53e8675249800d4e45e2382e5c95696889c46b
YSSkywalker <yukimurasakiskywalker940716@gmail.com>
2020-02-25 00:19:16 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Practice Virtual Keyboard using JS
+
+The purpose of this practice is to get knowladge for handle a virtual keyboard using JavaScript.
+I used "Simple-Keyboard".
+
+## Simple-Keyboard Info
+* version
+2.28.15
---
+++ src/index.js
@@ -0,0 +1,51 @@
+let Keyboard = window.SimpleKeyboard.default;
+
+let keyboard = new Keyboard({
+  onChange: input => onChange(input),
+  onKeyPress: button => onKeyPress(button),
+  layout: {
+    'default': [
+      '` 1 2 3 4 5 6 7 8 9 0 - = {bksp}',
+      '{tab} q w e r t y u i o p [ ] \\',
+      '{lock} a s d f g h j k l ; \' {enter}',
+      '{shift} z x c v b n m , . / {shift}',
+      'ushinohi.com @ {space}'
+    ],
+    'shift': [
+      '~ ! @ # $ % ^ & * ( ) _ + {bksp}',
+      '{tab} Q W E R T Y U I O P { } |',
+      '{lock} A S D F G H J K L : " {enter}',
+      '{shift} Z X C V B N M < > ? {shift}',
+      'ushinohi.com @ {space}'
+    ]
+  }
+});
+
+document.querySelector(".input").addEventListener("change", event => {
+  keyboard.setInput(event.target.value);
+});
+
+console.log(keyboard);
+
+function onChange(input) {
+  document.querySelector(".input").value = input;
+  console.log("Input changed", input);
+}
+
+function onKeyPress(button) {
+  console.log("Button pressed", button);
+
+  /**
+   * シフトで大文字入力を可能にする
+   */
+  if (button === "{shift}" || button === "{lock}") handleShift();
+}
+
+function handleShift() {
+  let currentLayout = keyboard.options.layoutName;
+  let shiftToggle = currentLayout === "default" ? "shift" : "default";
+
+  keyboard.setOptions({
+    layoutName: shiftToggle
+  });
+}
